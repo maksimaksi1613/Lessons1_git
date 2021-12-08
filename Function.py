c5c72284b63ccb8b1947aeb6d4b274c656ac65e5
Maksim <maksfucktor1996@mail.ru>
2021-12-08 16:05:10 +0630

    Add file in git

---
+++ Function.py
@@ -0,0 +1,10 @@
+def get_vat(payment, persent=20):
+	try:
+		payment = float(payment)
+		vat = payment / 100*persent
+		vat = round (vat,2)
+		return "Summ Vat: {}". format(vat)
+	except TypeError:
+		return "Can't count check data"
+result = get_vat(400, 15)
+print(result)
---
+++ Hello.py
@@ -0,0 +1,2 @@
+a=print(23)
+print (a)
